7b252783a217f001560be5583b094032a1ad2e5b
ssoj13 <96214515+ssoj13@users.noreply.github.com>
2025-06-25 10:15:08 -0700

    Fix cmakelists

--- _build.cmd
+++ _build.cmd
@@ -4,6 +4,6 @@ rem rd /s /q build
 mkdir build
 cd build
 rem set DESTDIR=C:\Programs\Lib
-cmake --log-level ERROR ../ -G "Visual Studio 17 2022" "-DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x64-windows" && cmake --build . --target ALL_BUILD --config Release
+cmake --log-level ERROR ../ -G "Visual Studio 17 2022" "-DVCPKG_TARGET_TRIPLET=x64-windows" && cmake --build . --target ALL_BUILD --config Release
 pause
 cmake -P cmake_install.cmake
